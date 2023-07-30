5f44247a73000d88035dfbde2b9709fe712c518e
Gerald Madlmayr <gerald.madlmayr@gmx.at>
2023-07-30 10:53:23 +0200

    Fix more #endifs

--- tasmota/user_config_override_sample.h
+++ tasmota/user_config_override_sample.h
@@ -90,18 +90,24 @@ Examples :
 #ifndef USE_SCRIPT
 #define USE_SCRIPT
 #endif
+
 #ifndef USE_SML_M
 #define USE_SML_M
 #endif
+
 #ifdef USE_RULES
 #undef USE_RULES
+
 #endif
 #ifndef USE_MQTT_TLS 
 #define USE_MQTT_TLS 
-#define USE_MQTT_TLS_CA_CERT // Optional but highly recommended #endif 
+#define USE_MQTT_TLS_CA_CERT // Optional but highly recommended
+#endif
+
 #ifndef USE_MQTT_AWS_IOT_LIGHT 
 #define USE_MQTT_AWS_IOT_LIGHT 
 #endif
+
 #ifdef USE_DISCOVERY 
 #undef USE_DISCOVERY
 #endif
